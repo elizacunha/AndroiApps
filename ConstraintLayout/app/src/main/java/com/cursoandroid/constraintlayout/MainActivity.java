c532eb46ea21ac987e8e870183279836bb295f5b
elizacunha <elizacunha@gmail.com>
2021-08-18 11:58:57 -0300

    Applicativo simples para exibir noticias

--- ConstraintLayout/app/src/main/java/com/cursoandroid/constraintlayout/MainActivity.java
+++ ConstraintLayout/app/src/main/java/com/cursoandroid/constraintlayout/MainActivity.java
@@ -9,7 +9,6 @@ import android.widget.TextView;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,13 +16,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    public void mudaTexto(View view) {
-        SimpleDateFormat formatoData = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss", Locale.US);
+
+        SimpleDateFormat formatoData = new SimpleDateFormat("dd-MM-yyyy");
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(new Date());
 
-        TextView texto = findViewById(R.id.textoExibido);
-        texto.setText("Hora atual: "+formatoData.format(calendar.getTime()));
+        TextView dataNoticia = findViewById(R.id.txtDataNoticia);
+        dataNoticia.setText(formatoData.format(new Date()));
+    }
+
+    public void onClickBotaoAnterior(View view) {
+       TextView txtTituloNoticia = findViewById(R.id.txtTituloNoticia);
+       txtTituloNoticia.setText(getResources().getString(R.string.txtTituloNoticia_1));
+       TextView txtNoticia = findViewById(R.id.txtNoticia);
+       txtNoticia.setText(getResources().getString(R.string.txtNoticia_1));
+        findViewById(R.id.btnAnterior).setEnabled(false);
+        findViewById(R.id.btnProximo).setEnabled(true);
+    }
+    public void onClickBotaoProximo(View view) {
+        TextView txtTituloNoticia = findViewById(R.id.txtTituloNoticia);
+        txtTituloNoticia.setText(getResources().getString(R.string.txtTituloNoticia_2));
+        TextView txtNoticia = findViewById(R.id.txtNoticia);
+        txtNoticia.setText(getResources().getString(R.string.txtNoticia_2));
+        findViewById(R.id.btnAnterior).setEnabled(true);
+        findViewById(R.id.btnProximo).setEnabled(false);
     }
 }
